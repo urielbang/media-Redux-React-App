a5701f58307141ba5b0f30a68519f48b1b772d90
urielbang <uri32655@gmail.com>
2023-12-21 18:07:11 +0200

    useThunk hook

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,30 +1,64 @@
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchUsers } from "../store";
+import { useSelector } from "react-redux";
+import { fetchUsers, addUser } from "../store";
 import Skeleton from "./Skeleton";
+import Button from "./Button";
+import { useThank } from "../hooks/use-thunk";
 
 function UsersList() {
-  const dispatch = useDispatch();
+  const [doFetchUsers, isLoadingUsers, loadingUsersError] =
+    useThank(fetchUsers);
 
-  const { isLoading, data, error } = useSelector((state) => {
+  const [doCreateUser, isCreatingUser, creatingUserError] = useThank(addUser);
+
+  const { data } = useSelector((state) => {
     return state.users;
   });
 
   useEffect(() => {
-    dispatch(fetchUsers());
-  }, [dispatch]);
+    doFetchUsers();
+  }, [doFetchUsers]);
 
-  if (isLoading) {
+  const handleUserAdd = () => {
+    doCreateUser();
+  };
+
+  if (isLoadingUsers) {
     return (
       <div>
         <Skeleton times={10} className="h-10 w-full" />
       </div>
     );
   }
-  if (error) {
+  if (loadingUsersError) {
     return <div>Error fectching data...</div>;
   }
 
-  return <div>{data.length}</div>;
+  const renderdUsers = data.map((user) => {
+    return (
+      <div key={user.id} className="mb-2 border rounded">
+        <div className="flex p-2 justify-between items-center cursor-pointer">
+          {user.name}
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <div className="flex flex-row justify-between m-3">
+        <h1 className="m-2 text-xl">Users</h1>
+        {isCreatingUser ? (
+          "Creating User..."
+        ) : (
+          <Button onClick={handleUserAdd} primary>
+            + Add User
+          </Button>
+        )}
+        {creatingUserError && "Error creating user...."}
+      </div>
+      {renderdUsers}
+    </div>
+  );
 }
 export default UsersList;
--- src/store/index.js
+++ src/store/index.js
@@ -7,3 +7,4 @@ export const store = configureStore({
   },
 });
 export * from "./thunks/fetchUsers";
+export * from "./thunks/addUser";
